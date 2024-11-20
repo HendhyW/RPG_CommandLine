4cb1f6c99b648001a5bd228182848c5bfba39ce9
Naufal Hadi Rasikhin <naufal.hdr153@gmail.com>
2024-11-20 22:10:51 +0700

    menambah gitignore

--- src/GameManager.cs
+++
@@ -1,8 +0,0 @@
-using system;
-
-public class GameManager
-{
-    //game manager berfungsi untuk mengatur bagaimana berjalannya game
-    //game manager akan mengatur navigasi player dalam map, memanggil battle system, menggunakan RNG  
-    
-}
